a62a7788c26d531fed56b66c529eaa5207baeeb2
Thirat <whalefrog@hotmail.com>
2016-10-27 20:10:36 +0700

    init

---
+++ app/src/main/java/com/example/whale_000/test/MainActivity.java
@@ -0,0 +1,40 @@
+package com.example.whale_000.test;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+
+    public void onButt(View v){
+        EditText number1 = (EditText) findViewById(R.id.number1);
+        EditText number2 = (EditText) findViewById(R.id.number2);
+    }
+    @Override
+    protected void onStart() {
+        super.onStart();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+    }
+}
